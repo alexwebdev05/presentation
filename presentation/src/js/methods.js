2f0e7c416cc7e5786597cc8602b9f24814902e16
Sniffsec <sniffsec@proton.me>
2024-04-09 11:34:42 +0200

    update

--- presentation/src/js/methods.js
+++
@@ -1,9 +0,0 @@
-export function scrollKnowledge() {
-    const knowledge = document.getElementById('knowledges')
-    knowledge.scrollIntoView({ behavior: 'smooth' })
-}
-
-import { langData } from '../lang/esp-eng'
-export function copy() {
-    navigator.clipboard.writeText(langData.contact.mail.contName)
-}
